c352b9207c9b1b31044cc38ee5412f993f8af52c
kusugawa <bhlzlx@hotmail.com>
2022-12-29 22:31:34 +0800

    update

--- core/declare.h
+++ core/declare.h
@@ -2,10 +2,21 @@
 
 namespace gui {
 
+    template<class T>
+    struct Point2D {
+        T x;
+        T y;
+    };
+
+    enum class MouseButton : int {
+        Left,
+        Right,
+        Middle,
+        Invalid,
+    };
     class Object;
     class Touch;
     class InputEvent;
-    class MouseButton;
     class EventDispatcher;
 
 }
